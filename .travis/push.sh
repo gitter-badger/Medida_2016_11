f9c77b9671abf7c30e8414ff4f0365d50cc73e4a
Fried.Ice <fried.ice.94@gmail.com>
2016-04-08 02:44:54 +0200

    improvements for git interactions

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,20 +4,22 @@ setup_git() {
   git config --global user.email "fried_ice@travis-ci.org"
   git config --global user.name "Travis CI"
   git remote add upstream https://${GH_TOKEN}@github.com/fried-ice/Medida_2016_11.git > /dev/null 2>&1
+  git fetch --all
 }
 
 commit_website_files() {
-  git fetch --all
-  git stash
-  git checkout doc-travis
-  git stash apply && git stash drop
+  git checkout doc
   cd doc/pdf
   git add . *.pdf
+  git stash
+  git checkout doc-travis
+  git checkout stash -- .
+  git stash drop
   git commit --message "Travis asciidoctor build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  
+
   git push --quiet upstream doc-travis
 }
 
