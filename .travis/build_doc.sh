bee55bb128b8f672e6a4c9c48ef17b3d761560ac
Fried.Ice <fried.ice.94@gmail.com>
2016-04-20 22:56:25 +0200

    exclude fragments in pdf build

--- .travis/build_doc.sh
+++ .travis/build_doc.sh
@@ -3,7 +3,7 @@
 src_dir="doc/"
 trg_dir="doc/pdf"
 
-for f in $(find $src_dir -type f -name "*.adoc"); do
+for f in $(find $src_dir -type f -name "*.adoc" ! -name "_*.adoc"); do
 	tmp="${f##*/}"
 	filename=${tmp%.*}
 	asciidoctor-pdf $f -o "$trg_dir/$filename.pdf";
