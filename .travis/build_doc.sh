95dd43d3bd465ee27e28db8d2c612945559c0e8e
Fried.Ice <fried.ice.94@gmail.com>
2016-04-22 17:46:01 +0200

    preparations for asciidoctor pdf theming

--- .travis/build_doc.sh
+++ .travis/build_doc.sh
@@ -2,9 +2,10 @@
 
 src_dir="doc/"
 trg_dir="doc/pdf"
+now="$(TZ='Europe/Berlin' date +'%d.%m.%Y')"
 
 for f in $(find $src_dir -type f -name "*.adoc" ! -name "_*.adoc"); do
 	tmp="${f##*/}"
 	filename=${tmp%.*}
-	asciidoctor-pdf $f -o "$trg_dir/$filename.pdf";
+	asciidoctor-pdf -a pdf-stylesdir=doc/themes -a pdf-style=blue -a pdf-fontsdir=doc/fonts -a localdate=$now $f -o "$trg_dir/$filename.pdf";
 done
