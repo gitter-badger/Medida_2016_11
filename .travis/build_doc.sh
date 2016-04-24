222d0fec8729d708a46a301861e0d7be5d08aec3
Fried.Ice <fried.ice.94@gmail.com>
2016-04-24 19:40:56 +0200

    PDFs now uploaded to Github Realeases for every Tag

--- .travis/build_doc.sh
+++ .travis/build_doc.sh
@@ -9,3 +9,6 @@ for f in $(find $src_dir -type f -name "*.adoc" ! -name "_*.adoc"); do
 	filename=${tmp%.*}
 	asciidoctor-pdf -a pdf-stylesdir=doc/themes -a pdf-style=blue -a pdf-fontsdir=doc/fonts -a localdate=$now $f -o "$trg_dir/$filename.pdf";
 done
+
+cd $trg_dir
+zip Dokumentation.zip *.pdf
